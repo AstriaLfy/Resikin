fe8720b1aa9595844102e51181b7ac36db98ba82
AstriaLfy <pravdaairen@gmail.com>
2025-03-04 19:46:03 +0700

    35% DONE

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,14 @@ class Halaman1 extends StatelessWidget {
             width: 420,
             height: 200,
             decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.6),
+                  spreadRadius: 2,
+                  blurRadius: 8,
+                  offset: Offset(0, 4),
+                ),
+              ],
               color: Colors.green,
               borderRadius: BorderRadius.only(
                 bottomLeft: Radius.circular(125.0),
@@ -26,27 +34,30 @@ class Halaman1 extends StatelessWidget {
               padding: const EdgeInsets.symmetric(horizontal: 30.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start, // Mengatur kolom ke kiri
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  SizedBox(height: 30),
                   Row(
                     children: [
                       Text(
                         "Halo, Raffi",
-                        style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
-                        textAlign: TextAlign.left, 
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        textAlign: TextAlign.left,
                       ),
 
-                      SizedBox(width: 140,),
+                      SizedBox(width: 140),
                       Icon(Icons.person), //ganti menjadi iconButton
-                      SizedBox(width: 20,),
-                      Icon(Icons.notifications)
-                      
+                      SizedBox(width: 20),
+                      Icon(Icons.notifications),
                     ],
                   ),
 
-                  SizedBox(height: 15),
+                  SizedBox(height: 25),
                   Center(
-                    child:   Container(
+                    child: Container(
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(30),
                         color: Colors.white,
@@ -60,11 +71,89 @@ class Halaman1 extends StatelessWidget {
                         ),
                       ),
                     ),
-                  )
-
+                  ),
                 ],
               ),
             ),
+          ),
+
+          new Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.grey,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 2,
+                  blurRadius: 8,
+                  offset: Offset(0, -4),
+                ),
+              ],
+            ),
+            height: 96,
+            width: 340,
+          ),
+
+          SizedBox(height: 30),
+
+          new Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white,
+              border: Border.all(color: Colors.black, width: 2.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 2,
+                  blurRadius: 8,
+                  offset: Offset(0, -4),
+                ),
+              ],
+            ),
+            height: 96,
+            width: 340,
+          ),
+
+          SizedBox(height: 30),
+
+          Row(
+            children: [
+              SizedBox(width: 35),
+              new Container(
+                height: 122,
+                width: 162,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black, width: 2.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.6),
+                      spreadRadius: 2,
+                      blurRadius: 8,
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(width: 20),
+              new Container(
+                height: 122,
+                width: 162,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black, width: 2.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.6),
+                      spreadRadius: 2,
+                      blurRadius: 8,
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ],
       ),
