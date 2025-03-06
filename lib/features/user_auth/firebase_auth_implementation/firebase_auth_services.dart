8aae727684dacd83ef603b026e767577f1da4908
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2025-03-06 22:51:51 +0700

    add: Validate email address

--- lib/features/user_auth/firebase_auth_implementation/firebase_auth_services.dart
+++ lib/features/user_auth/firebase_auth_implementation/firebase_auth_services.dart
@@ -1,28 +1,55 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseAuthServices {
-  FirebaseAuth _auth = FirebaseAuth.instance;
+  static final FirebaseAuthServices _instance = FirebaseAuthServices._internal();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<User?> signUpWithEmailAndPassword( String email, String password,) async {
+  factory FirebaseAuthServices() {
+    return _instance;
+  }
+
+  FirebaseAuthServices._internal();
+
+  //Validasi Email Address
+  bool validateEmailAddress(String email){
+    return RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email);
+  }
+
+  
+
+  Future<User?> signUpWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
     try {
-      UserCredential credential = await _auth.createUserWithEmailAndPassword( email: email, password: password);
+      UserCredential credential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
       return credential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak. Please try again');
+      }
     } catch (e) {
       print("some error occured");
-    } 
+    }
     return null;
   }
 
-
-  Future<User?> signInWithEmailAndPassword( String email, String password,) async {
+  Future<User?> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
     try {
-      UserCredential credential = await _auth.signInWithEmailAndPassword( email: email, password: password);
+      UserCredential credential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
       return credential.user;
     } catch (e) {
       print("some error occured");
-    } 
+    }
     return null;
   }
-
 }
-
