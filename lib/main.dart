f95663b6b3a3a2fe6e87a6711057f45113c9079f
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2025-03-11 22:42:41 +0700

    add: Terintegrasi dengan firebase

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ import 'page/beranda.dart';
 import 'page/onboarding_page.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform, 
   );
   runApp(MyApp());
@@ -17,8 +17,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Resikin',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: GoogleFonts.manropeTextTheme(
           Theme.of(context).textTheme,
